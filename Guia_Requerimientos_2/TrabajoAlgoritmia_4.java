450daae9f6a30fa1fd2d15e0b3932616743714fe
Juan-Wills <150960928+Juan-Wills@users.noreply.github.com>
2023-12-11 14:32:23 -0500

    Trabajo guia 2

---
+++ Guia_Requerimientos_2/TrabajoAlgoritmia_4.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class TrabajoAlgoritmia_2 {
+    public static void main(String[] args) {
+        try (Scanner sc= new Scanner(System.in)){
+            double dormir= 1.08;
+            double sentado= 1.66;
+            int peso_Kg= 70;
+            System.out.println("Tiempo registrado (en minutos)");
+              int tiempo= sc.next.Int();
+            System.out.println("Actividad fisica");
+            System.out.println("1. Dormir       2. Sentado      3. Ambos");
+              int opcion= sc.nextInt();
+          
+            switch(opcion){
+              case 1:
+                double calorias= (int)(dormir*tiempo);
+                int peso_rebajado= peso-calorias;
+                System.out.println("Ha quemado un total de " + peso_rebajado + " Kg");
+                break;
+                
+              case 2:
+                double calorias= (int)(sentado*tiempo);
+                int peso_rebajado= sentado-tiempo;
+                System.out.println("Ha quemado un total de " + peso_rebajado + " Kg");
+                break;
+                
+              case 3:
+                double calorias_sentado= (int)(sentado*tiempo);
+                double calorias_dormir= (int)(dormir*tiempo);
+                int peso_rebajado_sentado= sentado-tiempo;
+                int peso_rebajado_dormir= dormir-tiempo;
+                int total_calorias= peso_rebajado_sentado+peso_rebajado_dormir;
+                int peso_rebajado_total= peso-total_calorias;
+                System.out.println("Ha quemado un total de " + peso_rebajado_total + " Kg");
+                break;
+                
+              default:
+                break;
+            }
+        }
+    }
+}
