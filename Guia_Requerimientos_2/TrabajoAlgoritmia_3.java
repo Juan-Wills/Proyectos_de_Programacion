3d88bd03146e6b70717df2ae980beea1c8a802d9
Juan-Wills <150960928+Juan-Wills@users.noreply.github.com>
2023-12-11 15:02:13 -0500

    Trabajo guia 2

--- Guia_Requerimientos_2/TrabajoAlgoritmia_3.java
+++ Guia_Requerimientos_2/TrabajoAlgoritmia_3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TrabajoAlgoritmia_Final {                  //Sistema de pago de un producto
+public class TrabajoAlgoritmia_3 {                 
     public static void main(String args[]) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Escriba el primer numero");
