7568d7373ddbb40e7253a7669b301c7c5e9cc834
Juan-Wills <150960928+Juan-Wills@users.noreply.github.com>
2023-12-11 15:02:53 -0500

    Trabajo guia 2

--- Guia_Requerimientos_2/TrabajoAlgoritmia_5.java
+++ Guia_Requerimientos_2/TrabajoAlgoritmia_5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TrabajoAlgoritmia_2 {
+public class TrabajoAlgoritmia_5 {
     public static void main(String[] args) {
         try (Scanner sc= new Scanner(System.in)){
             String articulo= "(articulo X)";
