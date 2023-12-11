c2d3f0bdcd0441d17ae31d2c03e38477afd1a1b9
Juan-Wills <150960928+Juan-Wills@users.noreply.github.com>
2023-12-11 14:08:12 -0500

    Trabajo guia 2

--- Guia_Requerimientos_2/TrabajoAlgoritmia_3.java
+++ Guia_Requerimientos_2/TrabajoAlgoritmia_3.java
@@ -3,23 +3,18 @@ import java.util.Scanner;
 public class TrabajoAlgoritmia_Final {                  //Sistema de pago de un producto
     public static void main(String args[]) {
         try (Scanner sc = new Scanner(System.in)) {
-            int precio=30000;
-            System.out.println("*****************************************");
-            System.out.println("El precio del producto es de " + precio + " pesos.");  
-            System.out.println("*****************************************");
-            System.out.print("\nDigite la cantidad que desea llevar: ");
-            int cantidad= sc.nextInt();                                                                                                                                    //Implementacion del scanner
-            int total= precio*cantidad;
-            int descuento= total*0.20;
-            if (total>100000) {
-              int aplica_descuento= total-descuento;
-              System.out.println("\nSe acaba de aplicar un descuento del 20%  al valor de tu compra. El nuevo valor a pagar es de" + aplica_descuento + " pesos.");  //Descuento aplicado
-            } else if(total<=100000) {                                                                                                                    //Descuento no-aplicado
-                System.out.println("\nNo se aplica descuento. Por lo tanto, el valor de su compra sigue en "+ total + " pesos.");
-            }else{
-                System.out.print("\nError de digitacion, por favor vuelva a intentarlo.");                                                                              //Default
+            System.out.print("Escriba el primer numero");
+            int num1= sc.nextInt();
+            System.out.print("Escriba el segundo numero");
+            int num2= sc.nextInt();
+            if(num1>num2){
+                System.out.println(num1);
+                System.out.println(num2);
+            } else{
+                
+                System.out.println(num2);
+                System.out.println(num1);
             }
-            }    
-        }   
+        }
     }   
 }
